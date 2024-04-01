46fdc71410c896df83438bcd6252702ed12d274e
mifuda-tech <mifuda.tech@gmail.com>
2024-04-01 17:04:41 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -4,13 +4,25 @@ async function getData() {
   // 最初から表示されていると、クリアになりません。
   const userList = [
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 2,
+      first_name: '太郎',
+      family_name: '山田',
+      affiliation: 'HogeHoge大学',
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+
+  const result = await test(userList);
+  console.log(result);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const full_name = userList.map((data) => data.family_name + ' ' + data.first_name);
+      resolve(full_name);
+    }, 3000);
+  });
 }
-
